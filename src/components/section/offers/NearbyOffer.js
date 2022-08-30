f6f5e749c447e313cf8f30471535ef924c8d6447
kunal <13kunalthakur@gmail.com>
2022-08-30 18:37:39 +0530

    offers added

---
+++ src/components/section/offers/NearbyOffer.js
@@ -0,0 +1,144 @@
+import React from 'react'
+
+
+import './NearbyOffer.css';
+
+const NearbyOffer = () => {
+    return(
+        <>
+            <div className="nearbuy_offers section">
+                <h2 className='title'>Your Near Zone Offer 🎉  <a href="#">view offers</a></h2>
+                <div className="offers">
+                    <div className="offer_card">
+                        <div className="img_section">
+                            <img src={require('../../../assests/offers/offer1.png')} alt="offers" />
+                        </div>
+                        <div className="offer_details">
+                            <h2>Kill the bill</h2>
+                            <p className='categories'>Drinks and starters</p>
+                            <br />
+                            <p className='categories'>Drinks and starters</p>
+                            <hr className='line'/>
+                            <div className="offer_amount">
+                                <div className="price">
+                                    <del>&#8377;4700</del>
+                                    <p>&#8377;3500</p>
+                                </div>
+                                <div className="how_much_off">
+                                    <p>51% Off</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="offer_card">
+                        <div className="img_section">
+                            <img src={require('../../../assests/offers/offer1.png')} alt="offers" />
+                        </div>
+                        <div className="offer_details">
+                            <h2>Kill the bill</h2>
+                            <p className='categories'>Drinks and starters</p>
+                            <br />
+                            <p className='categories'>Drinks and starters</p>
+                            <hr className='line'/>
+                            <div className="offer_amount">
+                                <div className="price">
+                                    <del>&#8377;4700</del>
+                                    <p>&#8377;3500</p>
+                                </div>
+                                <div className="how_much_off">
+                                    <p>51% Off</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="offer_card">
+                        <div className="img_section">
+                            <img src={require('../../../assests/offers/offer1.png')} alt="offers" />
+                        </div>
+                        <div className="offer_details">
+                            <h2>Kill the bill</h2>
+                            <p className='categories'>Drinks and starters</p>
+                            <br />
+                            <p className='categories'>Drinks and starters</p>
+                            <hr className='line'/>
+                            <div className="offer_amount">
+                                <div className="price">
+                                    <del>&#8377;4700</del>
+                                    <p>&#8377;3500</p>
+                                </div>
+                                <div className="how_much_off">
+                                    <p>51% Off</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="offer_card">
+                        <div className="img_section">
+                            <img src={require('../../../assests/offers/offer1.png')} alt="offers" />
+                        </div>
+                        <div className="offer_details">
+                            <h2>Kill the bill</h2>
+                            <p className='categories'>Drinks and starters</p>
+                            <br />
+                            <p className='categories'>Drinks and starters</p>
+                            <hr className='line'/>
+                            <div className="offer_amount">
+                                <div className="price">
+                                    <del>&#8377;4700</del>
+                                    <p>&#8377;3500</p>
+                                </div>
+                                <div className="how_much_off">
+                                    <p>51% Off</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="offer_card">
+                        <div className="img_section">
+                            <img src={require('../../../assests/offers/offer1.png')} alt="offers" />
+                        </div>
+                        <div className="offer_details">
+                            <h2>Kill the bill</h2>
+                            <p className='categories'>Drinks and starters</p>
+                            <br />
+                            <p className='categories'>Drinks and starters</p>
+                            <hr className='line'/>
+                            <div className="offer_amount">
+                                <div className="price">
+                                    <del>&#8377;4700</del>
+                                    <p>&#8377;3500</p>
+                                </div>
+                                <div className="how_much_off">
+                                    <p>51% Off</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="offer_card">
+                        <div className="img_section">
+                            <img src={require('../../../assests/offers/offer1.png')} alt="offers" />
+                        </div>
+                        <div className="offer_details">
+                            <h2>Kill the bill</h2>
+                            <p className='categories'>Drinks and starters</p>
+                            <br />
+                            <p className='categories'>Drinks and starters</p>
+                            <hr className='line'/>
+                            <div className="offer_amount">
+                                <div className="price">
+                                    <del>&#8377;4700</del>
+                                    <p>&#8377;3500</p>
+                                </div>
+                                <div className="how_much_off">
+                                    <p>51% Off</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default NearbyOffer;
