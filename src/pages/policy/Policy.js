c070902f9e1b726e2ca04e6400043b22507f8ed1
kunal <13kunalthakur@gmail.com>
2022-09-07 10:26:20 +0530

    added our policy

---
+++ src/pages/policy/Policy.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './Policy.css';
+
+const Policy = () => {
+    return (
+        <>
+            <div className="about_section section">
+                <div className="di">
+
+                    <h2 className="cent">Terms &amp; Conditions</h2><br /><br />
+
+                </div>
+                <div className="warC">
+                    <div className="war2">
+                        <p>
+                            By accessing this website we assume you accept these terms
+                            and Conditions. Do not continue to use  OFFERSZONE website if you do not
+                            agree to take all of the terms and Conditions stated on this page
+                        </p>
+                    </div>
+
+                    <div className="list">
+                        <ul>
+                            <li>OffersZone is not involve with any Shop or Trade</li><br />
+                            <li>If you are considering any offer. Please consider on your behalf</li><br />
+                            <li>OffersZone is not Responsible for any Financial transactions </li><br />
+                            <li>If any trade is doing any fraud , we can take some Actions </li><br />
+                            <li>For other Queris.Please mail <span>helpdeskofferszone@gmail.com</span></li><br />
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+};
+
+export default Policy;
