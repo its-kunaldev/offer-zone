d1513952466e8a21bbcfc7e2d5600c121e171668
kunal <13kunalthakur@gmail.com>
2022-09-12 15:56:27 +0530

    handling router

--- src/pages/layout/Layout.js
+++ src/pages/layout/Layout.js
@@ -8,28 +8,26 @@ import Footer from '../../components/footer/Footer';
 import MainHeader from '../../components/header/MainHeader';
 import ModalWindow from '../../components/UI/modal window/ModalWindow';
 import Searches from '../../components/UI/search section/Searches';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom';
 
 const Layout = (props) => {
 
-    const history = useHistory();
-    console.log(history.location.pathname);
-
+    const location = useLocation();
 
     const isFocus = useSelector(state => state.uiSliceReducer.isFocus);
 
     return (
         <>
             <ModalWindow />
-            {isFocus && history.location.pathname !== '/signup' && history.location.pathname !== '/login' && <Searches />}
-            {!isFocus && history.location.pathname !== '/signup' && history.location.pathname !== '/login' && <MainHeader />}
+            {isFocus && location.pathname !== '/signup' && location.pathname !== '/login' && <Searches />}
+            {!isFocus && location.pathname !== '/signup' && location.pathname !== '/login' && <MainHeader />}
             <section>
                 {props.children}
             </section>
-            {history.location.pathname !== '/signup' && history.location.pathname !== '/login' && <div className="all_offer_btn">
+            {location.pathname !== '/signup' && location.pathname !== '/login' && <div className="all_offer_btn">
                 <Link to={'/all-offers'}>View All Offers</Link>
             </div>}
-            {history.location.pathname !== '/signup' && history.location.pathname !== '/login' && <Footer />}
+            {location.pathname !== '/signup' && location.pathname !== '/login' && <Footer />}
         </>
     )
 };
