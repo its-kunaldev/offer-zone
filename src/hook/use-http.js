0b61b9594ab9e43dc130517596ff2250a0b2c1ab
kunal <13kunalthakur@gmail.com>
2022-09-13 08:48:20 +0530

    custom hook

--- src/hook/use-http.js
+++ src/hook/use-http.js
@@ -1,41 +1,39 @@
 import { useReducer } from "react";
 import { useCallback } from "react";
 
-const initialState = {
-    data: null,
-    status: null,
-    error: null
-}
-
 const httpReducer = (state, action) => {
 
-    if(action.type === 'send'){
-        return {data: null, status: 'pending', error: null}
+    if (action.type === 'send') {
+        return { data: null, status: 'pending', error: null }
     }
-    if(action.type === 'success'){
-        return {data: action.data, status: 'completed', error: null}
+    if (action.type === 'success') {
+        return { data: action.data, status: 'completed', error: null }
     }
-    if(action.type === 'error'){
-        return {data: null, status: 'completed', error: action.error}
+    if (action.type === 'error') {
+        return { data: null, status: 'completed', error: action.error }
     }
-    return initialState;
+    return state;
 }
 
-const useHTTP = (fetchData) => {
+const useHTTP = (fetchData, startWithPending = false) => {
 
-    const [httpdata, dispatch] = useReducer(httpReducer, initialState);
+    const [httpdata, dispatch] = useReducer(httpReducer, {
+        data: null,
+        status: startWithPending ? 'pending' : null,
+        error: null
+    });
 
     const sendRequest = useCallback(async (requestedData) => {
-        dispatch({type: 'send'})
-        try{
-           const data = await fetchData(requestedData);
-        //    console.log(data);
-           dispatch({type: 'success', data});
+        dispatch({ type: 'send' })
+        try {
+            const data = await fetchData(requestedData);
+            //    console.log(data);
+            dispatch({ type: 'success', data });
         }
-        catch (error){
-            dispatch({type: 'error', error: 'something went wrong'});
+        catch (error) {
+            dispatch({ type: 'error', error: 'Something went wrong!!⚠️⚠️' });
         }
-        
+
     }, [fetchData]);
 
     return {
