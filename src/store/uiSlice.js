62489299345333d9fc2f0f7c3e2cb9fa86925ea9
kunal <13kunalthakur@gmail.com>
2022-09-13 13:22:32 +0530

    added more slice

--- src/store/uiSlice.js
+++ src/store/uiSlice.js
@@ -3,13 +3,19 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
     name: 'UI',
-    initialState: {showCitiesmodal: false, isFocus: false},
+    initialState: {showCitiesmodal: false, isFocus: false, isCredentialValid: true, isWarningRendered: false},
     reducers: {
         showModal(state){
             state.showCitiesmodal = !state.showCitiesmodal;
         },
         showSearchbar(state){
             state.isFocus = !state.isFocus;
+        },
+        checkValidity(state, action){
+            state.isCredentialValid = action.payload;
+        },
+        warningRender(state, action){
+            state.isWarningRendered = action.payload;
         }
     }
 });
